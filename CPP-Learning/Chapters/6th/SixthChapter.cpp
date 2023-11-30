85c4338d791a732d111386215fda300e26e39af0
AtennopVer <anton7185569@gmail.com>
2023-11-30 22:57:35 +0300

    Made 3rd task of 6th chapter

--- CPP-Learning/Chapters/6th/SixthChapter.cpp
+++ CPP-Learning/Chapters/6th/SixthChapter.cpp
@@ -65,3 +65,25 @@ void SixthChapter::RunSecondTask() const
 }
 
 //----------------------------------------------------------------------------------------------------
+void SixthChapter::RunThirdTask() const
+{
+    std::string temp_line;
+    std::cout << "Please enter one of the following choices: \n" <<
+        "c) carnivore   p) pianist\nt) tree        g) game\n";
+    
+    while (true)
+    {
+        std::cout << "Please enter a c, p, t or g: ";
+        std::getline(std::cin, temp_line);
+
+        if (temp_line == "c") std::cout << "Wolf is a carnivore";
+        else if (temp_line == "p") std::cout << "Pianist is a musical profession";
+        else if (temp_line == "t") std::cout << "Maple is a tree";
+        else if (temp_line == "g") std::cout << "Half-Life 2 is a great game";
+        else continue;
+
+        break;
+    }
+}
+
+//----------------------------------------------------------------------------------------------------
--- CPP-Learning/Main.cpp
+++ CPP-Learning/Main.cpp
@@ -6,7 +6,7 @@
 int main()
 {
     const auto chapter = new SixthChapter();
-    chapter->RunSecondTask();
+    chapter->RunThirdTask();
 
     free(chapter);
     return 0;
