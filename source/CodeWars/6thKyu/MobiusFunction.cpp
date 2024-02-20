3725b95340a0c3f8687e8ccec9b2c49c91447d1e
AtennopVer <anton7185569@gmail.com>
2024-02-20 21:39:07 +0300

    Fixed Mobius function

--- source/CodeWars/6thKyu/MobiusFunction.cpp
+++ source/CodeWars/6thKyu/MobiusFunction.cpp
@@ -1,47 +1,23 @@
-#include <vector>
+#include <valarray>
 
 namespace SixthKyu
 {
-    std::vector<long long int> CalculatePrimes(long long int n)
-    {
-        std::vector<long long int> result = { };
-        bool *all_numbers = new bool[n + 1] { };
-
-        for (long long int i = 0; i <= n; ++i)
-            all_numbers[i] = true;
-
-        for (long long int i = 2; i * i <= n; i++)
-            for (long long int j = i + 1; j != n; j++)
-                all_numbers[j] |= !(j % i);
-
-        for (long long int i = 2; i * i <= n; i++)
-            if (all_numbers[i])
-                result.push_back(i);
-
-        return result;
-    }
-    
     int Mobius(long long int n)
     {
-        std::vector<long long int> primes = CalculatePrimes(n);
-
-        for (long long int &prime: primes)
-            if (n % (prime * prime) == 0)
-                return 0;
-
         int count_of_factors = 0;
-        for (long long int &prime: primes)
+
+        for (int i = 2; i <= sqrt(n); i = (i == 2 ? 3 : i + 2))
         {
-            while (n % prime == 0)
+            if (n % i == 0)
             {
+                n /= i;
                 count_of_factors++;
-                n /= prime;
+
+                if (n % i == 0)
+                    return 0;
             }
         }
 
-        if (count_of_factors % 2 == 0)
-            return 1;
-
-        return -1;
+        return (count_of_factors % 2 == 0) ? -1 : 1;
     }
 }
--- source/main.cpp
+++ source/main.cpp
@@ -6,6 +6,6 @@
 
 int main()
 {
-    auto result = FourthKyu::Snail({{1,2,3,4}, {12,13,14,5}, {11,16,15,6}, {10,9,8,7}});
+    std::cout << SixthKyu::Mobius(6668);
     return 0;
 }
