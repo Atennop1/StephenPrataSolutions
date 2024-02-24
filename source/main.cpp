68611ce14fc7b294fe5c44dd18eca8ff710bea99
AtennopVer <anton7185569@gmail.com>
2024-02-24 18:07:48 +0300

    Moved LeetCode and CodeWars problems to new repo

--- source/main.cpp
+++ source/main.cpp
@@ -1,11 +1,7 @@
+#include "Declarations.h"
 #include <iostream>
-
-#include "StevenPrata/Declarations.h"
-#include "CodeWars/Declarations.h"
-#include "LeetCode/Declarations.h"
 
 int main()
 {
-    std::cout << SixthKyu::LastNonEmptyString("zzzzzzz");
     return 0;
 }
